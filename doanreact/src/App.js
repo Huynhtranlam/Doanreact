1e2b4269401d143c12cdb2dec6f6212c6044d42c
Huynhtranlam <huynhtranlam2009@gmail.com>
2024-08-20 11:06:34 +0700

    Set up

--- doanreact/src/App.js
+++ doanreact/src/App.js
@@ -1,25 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import CenteredDiv from './component/CenteredDiv.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CenteredDiv />
+      <Container /> /
     </div>
   );
+}
+
+
+
+function Container() {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  )
 }
 
 export default App;
---
+++ doanreact/src/component/CenteredDiv.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './CenteredDiv.css'; // Tạo một file CSS để áp dụng style
+
+const CenteredDiv = () => {
+    return (
+        <div className="centered-div">
+
+        </div>
+    );
+}
+
+export default CenteredDiv;
